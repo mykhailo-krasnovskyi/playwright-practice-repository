4bddcf468950afc86730d7627491e091c6b81b61
Michael Krasnovskyi <michael.krasnovskyi@gmail.com>
2025-02-03 22:09:09 +0200

    lesson 30 code added

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Base Image
+FROM mcr.microsoft.com/playwright:v1.50.1-noble
+# Set the working directory
+WORKDIR /app
+# Copy package.json and package-lock.json
+COPY package*.json ./
+# Install dependencies
+RUN npm install
+# Copy the rest of the application files
+COPY . .
+
+RUN npx playwright install
+# Set the entry point for the container
+CMD ["npx", "playwright", "test", "garage"]
--- playwright.config.ts
+++ playwright.config.ts
@@ -21,9 +21,11 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['list']],
+  reporter: [['html', {
+    open: 'never'
+  }], ['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
